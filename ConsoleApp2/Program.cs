96136dff6173359c4519908957d3f7439b29d860
Omar Ahmad <11011657@stud.hochschule-heidelberg.de>
2020-08-21 12:48:55 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Gib Zahl");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Gib Zahl2");
+            //int y = int.Parse(Console.ReadLine());
+
+            //int z = x + y;
+            //Console.WriteLine("Zahl + Zahl2 ist {0}",z);
+
+            //const int x = 5;
+            //Console.WriteLine("Gib Zahl");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Gib Zahl2");
+            //int z = int.Parse(Console.ReadLine());
+
+            //int x1 = y * z;
+            //int x2 = x1 * x;
+
+            //Console.WriteLine("Es sind {0} m^2 und kostet dich {1} euro",x1,x2);
+
+            //Console.WriteLine("Gib Minuten");
+            //int x = int.Parse(Console.ReadLine());
+
+            //int y = x / 60;
+            //int z = x % 60;
+
+            //Console.WriteLine("du opferst {0} Stunden und {1} Minuten deines lebens", y, z);
+
+            //int x = 5;
+            //int y = 6;
+
+            ////(condition) ? true : false
+            //int z = x > y ? x : y;
+
+            //Console.WriteLine("studenrate");
+            //double x = double.Parse(Console.ReadLine());
+
+
+
+            //if (7.5<x && x<49)
+            //{
+            //    Console.WriteLine("rate ist okay");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("rate nicht okay");
+            //}
+            //int b = 20;
+
+            //Console.WriteLine("länge");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.WriteLine("breite");
+            //double y = double.Parse(Console.ReadLine());
+
+            //double z = x * y;
+
+            //if (z<400)
+            //{
+            //    int a = 25;
+            //    double c = a * z;
+            //    double d = c * b;
+            //    Console.WriteLine("dein feld ist {0} m^2 groß es kostet dich {1}€ die woche und 20 wochen kosten {2}", z, c, d);
+            //}
+            //else if (z>=400 && z<600)
+            //{
+            //    int a = 35;
+            //    double c = a * z;
+            //    double d = c * b;
+            //    Console.WriteLine("dein feld ist {0} m^2 groß es kostet dich {1}€ die woche und 20 wochen kosten {2}", z, c, d);
+            //}
+            //else if (z>=600)
+            //{
+            //    int a = 50;
+            //    double c = a * z;
+            //    double d = c * b;
+            //    Console.WriteLine("dein feld ist {0} m^2 groß es kostet dich {1}€ die woche und 20 wochen kosten {2}", z, c, d);
+            //}
+
+            //Random randomGenerator = new Random();
+            //int randomNumber = randomGenerator.Next(1, 11);
+
+            //Console.WriteLine("gib zahl zwischen 1 und 10");
+            //int x = int.Parse(Console.ReadLine());
+
+            //    if (x == randomNumber)
+            //{
+            //    Console.WriteLine("richtig!!{0}", randomNumber);
+            //}
+            //else if (x>randomNumber)
+            //{
+            //    Console.WriteLine("zu hoch{0}", randomNumber);
+            //}
+            //else if (x<randomNumber)
+            //{
+            //    Console.WriteLine("zu niedrig {0}",randomNumber);
+            //}
+
+            //for (int i = 0; 0 < 5; i++) ;
+            //double x;
+            //do
+            //{
+            //    Console.WriteLine("enter payrate");
+            //    x = double.Parse(Console.ReadLine());
+            //} while (x < 5.65 || x > 49.99);
+
+            //Console.WriteLine("your payrate is {0}", x);
+            //int k = 1;
+            //for (int i = 2; i < 51; i++)
+            //{
+            //    k += i;
+            //};
+            //Console.WriteLine(k);
+
+            //Random randomGenerator = new Random();
+            //int randomNumber = randomGenerator.Next(1, 11);
+
+            //int x;
+
+            //do
+            //{
+
+            //    Console.WriteLine("gib zahl zwischen 1 und 10");
+            //    x = int.Parse(Console.ReadLine());
+
+
+
+
+            //    //if (x == randomNumber)
+            //    //{
+            //    //    Console.WriteLine("richtig!!{0}", randomNumber);
+            //    //}
+            //     if (x > randomNumber)
+            //    {
+            //        Console.WriteLine("zu hoch");
+            //    }
+            //     if (x < randomNumber)
+            //    {
+            //        Console.WriteLine("zu niedrig ");
+            //    }
+            //}
+            //while (x != randomNumber);
+            //Console.WriteLine("richtig!!{0}", randomNumber);
+
+
+            //int[] myArray = new int[3];
+            //myArray[0] = 5;
+
+            //int[] myArray2 = new int[3] { 4, 5, 6 };
+            //int[] myArray3 = new int[] { 4, 5, 6 };
+            //int[] myArray4 = { 4, 5, 6 };
+
+
+            //Console.WriteLine(myArray[2]);
+
+
+
+
+
+
+        }
+    }
+}
